4e4c43f4469ce1569a7fe3715058bb8e934adab3
Prestijhonny <wprestigiovanni@yahoo.it>
2024-01-15 13:57:13 +0100

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -2,5 +2,5 @@
 
 make
 gnome-terminal -- bash -c "cd server/src;./server; exec bash"
-gnome-terminal -- bash -c "cd client/src;./client; exec bash"
-gnome-terminal -- bash -c "cd client/src;./client; exec bash"
+gnome-terminal -- bash -c "cd client/src;./client << Ciao dal client 1; exec bash"
+gnome-terminal -- bash -c "cd client/src;./client << Ciao dal client 2; exec bash"
