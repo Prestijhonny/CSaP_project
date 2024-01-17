865b1bdca331adfb11b57c8764489dee66043f83
Prestijhonny <50366546+Prestijhonny@users.noreply.github.com>
2024-01-17 14:13:16 +0100

    Update autorun.sh

--- startup/autorun.sh
+++ startup/autorun.sh
@@ -2,5 +2,5 @@
 
 make
 gnome-terminal -- bash -c "cd server/src;./server; exec bash"
-gnome-terminal -- bash -c "cd client/src;echo -e "Ciao dal client 1!" | ./client; exec bash"
-gnome-terminal -- bash -c "cd client/src;echo -e "Ciao dal client 2!" | ./client; exec bash"
+gnome-terminal -- bash -c "cd client/src;./client < input1; exec bash"
+gnome-terminal -- bash -c "cd client/src;./client < input2; exec bash"
