10e0c6536625fc233ce41656a644b904f5da152c
Prestijhonny <50366546+Prestijhonny@users.noreply.github.com>
2024-01-17 14:15:02 +0100

    Update autorun.sh

--- startup/autorun.sh
+++ startup/autorun.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+cd ../
 make
 gnome-terminal -- bash -c "cd server/src;./server; exec bash"
-gnome-terminal -- bash -c "cd client/src;./client < input1; exec bash"
-gnome-terminal -- bash -c "cd client/src;./client < input2; exec bash"
+gnome-terminal -- bash -c "cd client/src;./client < ../../startup/input1; exec bash"
+gnome-terminal -- bash -c "cd client/src;./client < ../../startup/input2; exec bash"
