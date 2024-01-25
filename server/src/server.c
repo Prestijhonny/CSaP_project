863f9a2916d9c82730094c02b9884f09b4f552bb
Prestijhonny <50366546+Prestijhonny@users.noreply.github.com>
2024-01-25 16:24:56 +0100

    Update server.c

--- server/src/server.c
+++ server/src/server.c
@@ -54,6 +54,16 @@ int main(int argc, char *argv[])
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(PORT);
 
+    int opt = 1;
+    // Setup some options for current socket, SO_REUSEADDR is an option that permit to reuse immediately a local address, although it is still used by another socket
+    // SOL_SOCKET indicates the level argument specifies the protocol level at which the option resides
+    if ((setsockopt(sockfd,SOL_SOCKET, SO_REUSEADDR,(char *)&opt, sizeof(opt))) == -1){
+        printf("Error to setup options to socket\n");
+        shutdown(sockfd,SHUT_RDWR); 
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    
     // Bind the socket
     if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) == -1)
     {
